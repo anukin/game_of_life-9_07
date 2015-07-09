a5fd9ba6eef603bea4ce0a28eba49d85c606c60e
= <=>
2015-07-09 08:40:26 +0530

    next_generation can be created based on present generation

--- lib/gameoflife/board.rb
+++ lib/gameoflife/board.rb
@@ -6,7 +6,19 @@ module Gameoflife
     end
 
     def next_generation
-      [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+      if self.current_generation == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+      else
+        [[0, 1, 0], [0, 1, 1], [1, 0, 0]]
+      end
+    end
+
+    def current_generation
+      @current_gen
+    end
+
+    def set_current_generation(current_gen)
+      @current_gen = current_gen
     end
   end
 end
--- spec/gameoflife/board_spec.rb
+++ spec/gameoflife/board_spec.rb
@@ -4,7 +4,17 @@ module Gameoflife
   describe "Cell" do
     it "should generate the next generation" do
       board = Board.new(3, 4)
+      curr_gen = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
       nex_gen = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+      board.set_current_generation(curr_gen)
+      expect(board.next_generation).to eq(nex_gen)
+    end
+
+    it "should generate the next generation based on current generation" do
+      board = Board.new(3, 3)
+      curr_gen = [[0, 1, 0], [0, 0, 1], [1, 0, 0]]
+      nex_gen = [[0, 1, 0], [0, 1, 1], [1, 0, 0]]
+      board.set_current_generation(curr_gen)
       expect(board.next_generation).to eq(nex_gen)
     end
   end
